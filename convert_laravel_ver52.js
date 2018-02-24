497c6d192300420a070c5d8124879c2d650e1513
abetomoki <catttm24@gmail.com>
2018-02-24 06:18:47 +0000

    laravel5.devの場合には遷移させないように修正しました。

--- convert_laravel_ver52.js
+++ convert_laravel_ver52.js
@@ -1,9 +1,12 @@
-window.onload = function() {
-    var targetLaravelVersion = "/laravel/5.2/";
-    var url = location.pathname;
-    var destination = url.slice(13);
-    if (url.indexOf(targetLaravelVersion) !== 0) {
-         window.location.href = targetLaravelVersion + destination;
-    }
-};
+ window.onload = function() {
+     var ver52 = "/laravel/5.2/";
+     var ver5Dev = "/laravel/5.dev/";
+     var url = location.pathname;
+     var destination = url.slice(13);
 
+     // バージョン5.2 or 5.dev以外の場合に5.2へ遷移
+     if (url.indexOf(ver52) !== -1 || url.indexOf(ver5Dev) !== -1) {
+         exit;
+     }
+     window.location.href = ver52 + destination;
+ };
